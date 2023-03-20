e68e4818fcd3dafa6cc9691c3a7cfce5ed1b8641
mihretu endeshaw <mihretuendeshaw84@gmail.com>
2023-03-20 14:20:50 +0300

    kk

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbarcomp from "./components/Navbarcomp";
-
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element= {<Home />}/>
       </Routes>
+      <Footer />
       </BrowserRouter>
     </div>
   
