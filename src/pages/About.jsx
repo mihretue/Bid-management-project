db346247965c115924a3e499c8c524f12140b1a8
wubeabera123 <93040439+wubeabera123@users.noreply.github.com>
2023-05-22 12:06:34 +0000

    privacy

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,8 +17,17 @@ Our team is dedicated to providing exceptional customer service and support to e
 
 Thank you for considering our bid management system. We look forward to working with you to streamline your bidding process and increase your chances of winning more contracts.</p>
       </div>
-      <div>
-           
+      <div className='container border rounded'>
+         <h2>Privacy Policy</h2>
+            <p>
+            
+
+1. At PPa, we're passionate about helping businesses succeed in the world of online advertising. That's why we've developed a powerful bid management system that makes it easy to optimize your campaigns and drive better results. Our team of experts is dedicated to providing top-notch support and guidance every step of the way, so you can be confident that you're making the most of your advertising budget.
+
+2. If you're tired of spending countless hours managing your online ad campaigns, [Company Name] is here to help. Our bid management system automates many of the most time-consuming tasks, so you can focus on what you do best – running your business. With advanced analytics and reporting tools, you'll have complete visibility into your campaigns and be able to make data-driven decisions that drive real results.
+
+3. As a leading provider of bid management solutions, [Company Name] is committed to helping businesses of all sizes succeed in the world of online advertising. Our platform is designed to be intuitive and user-friendly, so you can quickly and easily optimize your campaigns for maximum ROI. Whether you're new to online advertising or a seasoned pro, our team of experts is here to provide personalized support and guidance whenever you need it.
+            </p>
       </div>
       </div>
     )
