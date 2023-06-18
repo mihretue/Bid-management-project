09162de0a34e9344682650d201e39c7df06cdc69
meslody <mesfinlodamo4@gmail.com>
2023-06-18 14:05:28 +0300

    changes

--- src/pages/user action pages/bidder/bidsinprogress.jsx
+++ src/pages/user action pages/bidder/bidsinprogress.jsx
@@ -16,7 +16,7 @@ const BidsInProgress=()=>{
         <div className="p-2 w-100 fluid" style={{minHeight:'2rem'}}>
            <a className="icon-link text-decoration-none text-black">
             <BsArrowLeft className='me-2' />
-            <Link className="text-decoration-none" to={`/userpage/supplier/${id}`}>Back to manage bids</Link>
+            <Link className="text-decoration-none" to={`/userpage/supplier/${id}`}>Back to manage </Link>
            </a>
         </div>
         <StickyHeadTable />
