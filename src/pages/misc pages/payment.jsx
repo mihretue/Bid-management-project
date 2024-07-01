9ba81c91e188015d342de9243129d9d4d67df28c
dagimassefa <kuku.assefa18@gmail.com>
2024-07-01 13:04:19 +0300

    done with payment integration

--- src/pages/misc pages/payment.jsx
+++ src/pages/misc pages/payment.jsx
@@ -1,97 +1,17 @@
-// import { useState } from "react";
-// import Footer from '../../components/footer'
-// import Pdf from "../../resources/PDF_file_icon.png"
-// import { useEffect } from "react";
-// import {
-//     PayPalScriptProvider,
-//     PayPalButtons,
-//     usePayPalScriptReducer
-// } from "@paypal/react-paypal-js";
-// import { Button } from "evergreen-ui";
-// const amount = "12.99";
-// const currency = "USD";
-// const style = {"layout":"vertical"};
-
-// const ButtonWrapper = ({ currency, showSpinner }) => {
-//     const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
-
-//     useEffect(() => {
-//         dispatch({
-//             type: "resetOptions",
-//             value: {
-//                 ...options,
-//                 currency: currency,
-//             },
-//         });
-//     }, [currency, showSpinner]);
-//     return (<>
-//             { (showSpinner && isPending) && <div className="spinner" /> }
-//             <PayPalButtons
-//                 style={style}
-//                 disabled={false}
-//                 forceReRender={[amount, currency, style]}
-//                 fundingSource={undefined}
-//                 createOrder={(data, actions) => {
-//                     return actions.order
-//                         .create({
-//                             purchase_units: [
-//                                 {
-//                                     amount: {
-//                                         currency_code: currency,
-//                                         value: amount,
-//                                     },
-//                                 },
-//                             ],
-//                         })
-//                         .then((orderId) => {
-//                             return orderId;
-//                         });
-//                 }}
-//                 onApprove={(data, actions)=> {
-//                     return actions.order.capture().then(function (details) {
-//                         document.getElementById('success_btn').hidden=false;
-//                       });
-                      
-//                 }}
-//                 />
-//         </>
-//     );
-// }
-// export default function Payment() {
-//   const navigate = useNavigate();
-// 	return (
-// 		<div className="mt-3" style={{ maxWidth: "20rem", minHeight: "10rem" }}>
-//             <PayPalScriptProvider
-//                 options={{
-//                     "clientId": "ARbsOoxVAQUJH9tXj65Ty3VaAtdv_5wOWknAOLGboMhexc1GImhoUG1qMpfskJMhqY6iAIMcqCuyXsKN",
-//                     components: "buttons",
-//                     currency: "USD"
-//                 }}
-//             >
-// 				<ButtonWrapper
-//                     currency={currency}
-//                     showSpinner={false}
-//                 />
-//                 <Button hidden id="success_btn" onClick={() => {navigate(`/userpage/supplier/${id}/tenders/${tid}/apply/bid-document`)}}>
-//                   Continue
-//                 </Button>
-// 			</PayPalScriptProvider>
-// 		</div>
-// 	);
-// }
-import React, { useState, useEffect } from 'react';
-import { Link,useParams,useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 const Payment = () => {
   const [paymentComplete, setPaymentComplete] = useState(false);
-  const {tid,id} = useParams();
+  const { tid, id } = useParams();
 
   useEffect(() => {
     // Initialize the PayPal script
     const initializePayPalScript = () => {
-      const script = document.createElement('script');
-      script.src = 'https://www.paypal.com/sdk/js?client-id=ARbsOoxVAQUJH9tXj65Ty3VaAtdv_5wOWknAOLGboMhexc1GImhoUG1qMpfskJMhqY6iAIMcqCuyXsKN'; // Replace with your PayPal Client ID
-      script.addEventListener('load', () => handlePaymentButton());
+      const script = document.createElement("script");
+      script.src =
+        "https://www.paypal.com/sdk/js?client-id=ARbsOoxVAQUJH9tXj65Ty3VaAtdv_5wOWknAOLGboMhexc1GImhoUG1qMpfskJMhqY6iAIMcqCuyXsKN"; // Replace with your PayPal Client ID
+      script.addEventListener("load", () => handlePaymentButton());
       document.body.appendChild(script);
     };
 
@@ -108,7 +28,7 @@ const Payment = () => {
             purchase_units: [
               {
                 amount: {
-                  value: '10.00', // Replace with the desired payment amount
+                  value: "10.00", // Replace with the desired payment amount
                 },
               },
             ],
@@ -123,7 +43,7 @@ const Payment = () => {
           });
         },
       })
-      .render('#paypal-button-container'); // Replace with the container ID where you want to render the PayPal button
+      .render("#paypal-button-container"); // Replace with the container ID where you want to render the PayPal button
   };
 
   return (
